724cd49e04a315aaa130d1f5b620b15d6fb696e5
soroosh <soroosh.mortazavi@gmail.com>
2020-04-01 18:44:16 +0430

    write code in app.js file for using Weather.js module and get a args in the cli form the user

---
+++ app.js
@@ -0,0 +1,18 @@
+const Weather = require('./WeatherApi');
+const chalk = require('chalk');
+// this used for get the third arg in the comand line.
+var command = process.argv[2];
+
+switch (command) {
+    case 'current':
+        Weather.getCurrentWeather(process.argv[3])
+        break;
+    case 'forecast':
+        Weather.getForecastWeather(process.argv[3])
+        break;
+
+    default:
+        console.log(chalk.yellow("wrong command !!! \n example 1: node app forecast cityName \n "+
+        "example 2: node app current cityName") )
+        break;
+}
